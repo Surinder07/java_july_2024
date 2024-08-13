75b58476aa79e70d53c63197ea3f26efa8323ad6
sunnycode07 <sunny@pragra.io>
2024-08-12 20:32:49 -0400

    arrays max and min

---
+++ out/production/Java_july_2024/aug12/readme.md
@@ -0,0 +1,20 @@
+## Recap 
+
+Arrays. 
+what is an array ?
+Collection of similar data type 
+
+
+int[] arr;
+int arr[];
+int []arr;
+
+
+static 
+1. static variable 
+2. static method 
+3. static blocks 
+4. static inner .. Later [inner class]
+
+
+
---
+++ src/aug12/Test.java
@@ -0,0 +1,37 @@
+package aug12;
+
+public class Test {
+    public static void main(String[] args) {
+
+        /*int[] arr;
+        int myArray[];
+        int []arrr;
+       // []int a;*/
+
+
+
+        int[] arr = {10,20,30,40,50};
+/*
+        for (int i = 0; i < arr.length ; i++) {
+            System.out.println(arr[i]);
+        }*/
+
+
+        // for each loop
+// for every integer element in arr , print that element
+        for (int element : arr) {
+            System.out.println(element);
+        }
+
+
+        // reverse an array
+
+        //
+
+
+
+
+
+
+    }
+}
